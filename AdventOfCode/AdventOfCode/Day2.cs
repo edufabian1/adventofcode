12f482366faee23abb3d34f1dea2b537d26580ee
edufabian <eduardo.guanco@kdsoft.io>
2019-12-03 21:40:23 -0300

    Agrego solucion 2

---
+++ AdventOfCode/AdventOfCode/Day2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode
+{
+    [TestClass]
+    public class Day2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
