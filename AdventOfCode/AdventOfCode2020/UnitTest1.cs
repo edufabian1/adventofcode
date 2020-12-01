4b20d86443b60e855e891018d98e01e02966b761
Eduardo Guanco <eduardo.guanco@kdsoft.io>
2020-12-01 20:20:53 -0300

    refactor y mejora en tiempos de ejecucion, se mantiene doble for

--- AdventOfCode/AdventOfCode2020/UnitTest1.cs
+++ AdventOfCode/AdventOfCode2020/UnitTest1.cs
@@ -9,47 +9,44 @@ namespace AdventOfCode2020
 {
     public class Tests
     {
+        private List<string> _lstLines;
+
         [SetUp]
         public void Setup()
         {
+            string[] lines = File.ReadAllLines(@"D:\Proyectos\proyectos-personales\adventofcode\AdventOfCode\AdventOfCode2020\Resources\input1.txt");
+            //string[] lines = new string[] { "1721", "979", "366", "299", "675", "1456" };
+            
+            _lstLines = lines.OfType<string>().ToList();
         }
 
         [Test]
         public void Test1()
         {
-            string[] lines = File.ReadAllLines(@"D:\Proyectos\proyectos-personales\adventofcode\AdventOfCode\AdventOfCode2020\Resources\input1.txt");
-            //string[] lines = new string[] { "1721", "979", "366", "299", "675", "1456" };
-            
-            List<string> lstLines = lines.OfType<string>().ToList();
+            int result = Search(_lstLines, 2020);
 
-            int result = Search(lstLines, 2020);
-
-            Assert.Pass(result.ToString());
+            Console.WriteLine("Result: " + result);
+            Assert.IsNotNull(result.ToString());
         }
 
         [Test]
         public void Test2()
         {
-            string[] lines = File.ReadAllLines(@"D:\Proyectos\proyectos-personales\adventofcode\AdventOfCode\AdventOfCode2020\Resources\input1.txt");
-            //string[] lines = new string[] { "1721", "979", "366", "299", "675", "1456" };
-            int result = 0;
+            int result = 0, resultPartial = 0;
 
-            List<string> lstLines = lines.OfType<string>().ToList();
-
-            for (int i = 0; i < lstLines.Count(); i++)
+            for (int i = 0; i < _lstLines.Count(); i++)
             {
-                int resultPartial = 2020 - int.Parse(lstLines[i]);
-                List<string> lstPartial = lstLines.GetRange(i + 1, lstLines.Count() - (i + 1));
+                resultPartial = 2020 - int.Parse(_lstLines[i]);
+                
+                List<string> lstPartial = _lstLines.GetRange(i + 1, _lstLines.Count() - (i + 1));
+                
                 result = Search(lstPartial, resultPartial);
                 
-                if (result != 0)
-                {
-                    result *= int.Parse(lstLines[i]);
-                    break;
-                }
-            }            
+                if (result != 0) break;
+            }
 
-            Assert.Pass(result.ToString());
+            Console.WriteLine("Result: " + result * (2020 - resultPartial));
+            Assert.IsNotNull(result.ToString());
         }
 
         private int Search(List<string> lstLines, int search)
@@ -60,10 +57,9 @@ namespace AdventOfCode2020
             {
                 resultPartial = search - int.Parse(lstLines[i].Trim().ToString());
 
-                var partial = lstLines.Find(x => x == resultPartial.ToString());
-
-                if (!string.IsNullOrEmpty(partial))
-                    result = int.Parse(partial.Trim().ToString()) * int.Parse(lstLines[i].Trim().ToString());
+                for (int j = i + 1; j <= lstLines.Count() - 1; j++)
+                    if (lstLines[j].Trim().ToString().Equals(resultPartial.ToString()))
+                        return resultPartial * int.Parse(lstLines[i].Trim().ToString());
             }
             return result;
         }
